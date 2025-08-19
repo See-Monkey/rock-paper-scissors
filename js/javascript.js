41ef7fe0bedc3febb9f1272b29fb420baf0f6203
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-08-18 20:54:18 -0600

    Add function getComputerChoice

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    if (random == 0) {
+        return "rock";
+    }
+    else if (random == 1) {
+        return "paper";
+    }
+    else if (random == 2) {
+        return "scissors"
+    }
+    else {
+        return "error"
+    }
+}
+
+console.log(getComputerChoice());
